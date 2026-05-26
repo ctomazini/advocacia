c80b7279feeb1d4afea29fca0b2721f654804f27
Charles Tomazini <charles.tomazini@gmail.com>
2026-05-26 22:06:10 +0000

    feat: Custom Fields no Customer (RG, nacionalidade, estado civil, profissão) + fixtures atualizados

---
+++ advocacia/hooks.py
@@ -0,0 +1,17 @@
+app_name = "advocacia"
+app_title = "Advocacia"
+app_publisher = "Charles Tomazini"
+app_description = "Gestao juridica para escritorios de advocacia"
+app_email = "charles.tomazini@gmail.com"
+app_license = "mit"
+
+fixtures = [
+    {
+        "dt": "Custom Field",
+        "filters": [["dt", "in", ["Sales Invoice", "Customer"]], ["fieldname", "like", "custom_%"]]
+    },
+    {
+        "dt": "Server Script",
+        "filters": [["name", "in", ["Gerar Faturas Acordo", "Atualizar Faturas Acordo", "Contar Faturas Acordo", "Gerar Faturas Atos"]]]
+    }
+]
